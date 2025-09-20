b57ea4ca007ca08f7acb7b907878a8694d8da15e
Amar Dhillon <87444806+AmarDhillon05@users.noreply.github.com>
2025-09-20 11:54:07 -0400

    Update rate.py

--- pennapps/rate.py
+++ pennapps/rate.py
@@ -21,11 +21,13 @@ df = pd.read_csv("crime_2025.csv")
 
 def get_closest(df, lat, long):
     dists = []
-    cutoff = 100
     for lat2, lon2 in zip(df.lat, df.lng):
-        dists.append(meters_dist(lat, lat2, long, lon2) <= cutoff)
+        dists.append(meters_dist(lat, lat2, long, lon2))
     
-    copy = df[pd.Series(dists)]
+    copy = df.copy()
+    copy['dist'] = dists
+    copy = copy.sort_values(by = 'dist').loc[5, :]
+
     if str(copy['danger_code']).lower() == 'nan':
         copy['danger_code'] = [danger_scores[i] if i in danger_scores else 3 for i in copy.text_general_code]
 
